ce099a9f50c55d35fd657a854b719573dcee1370
hurtcraft <abeuhkclefeuh@gmail.com>
2025-10-14 14:25:54 +0200

    first commit

---
+++ src/main/java/client/asta_lsi2/AstaLsi2Application.java
@@ -0,0 +1,14 @@
+package client.asta_lsi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstaLsi2Application {
+
+    public static void main(String[] args) {
+        System.out.println("tata");
+        SpringApplication.run(AstaLsi2Application.class, args);
+    }
+
+}
---
+++ src/test/java/client/asta_lsi2/AstaLsi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package client.asta_lsi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstaLsi2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
