56cee1de2eb19893762ac4b9f6f6701f971f5cda
VeKTyS <gtastyle.731@gmail.com>
2025-10-18 15:03:38 +0200

    Ajouter affichage du tableau de bord des apprentis pour l'année en cours, message rouge si la liste est vide et endpoint pour ajouter un apprenti de test.

--- src/main/java/client/asta_lsi2/repository/ApprentiRepository.java
+++ src/main/java/client/asta_lsi2/repository/ApprentiRepository.java
@@ -1,4 +1,16 @@
 package client.asta_lsi2.repository;
 
-public class ApprentiRepository {
+import client.asta_lsi2.models.Apprenti;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.Year;
+import java.util.List;
+
+public interface ApprentiRepository extends JpaRepository<Apprenti, Long> {
+
+	@Query("SELECT a FROM Apprenti a WHERE a.apprenti_year = :year")
+	List<Apprenti> findByYear(@Param("year") Year year);
+
 }
--- src/main/java/client/asta_lsi2/service/ApprentiService.java
+++ src/main/java/client/asta_lsi2/service/ApprentiService.java
@@ -1,4 +1,27 @@
 package client.asta_lsi2.service;
 
+import client.asta_lsi2.models.Apprenti;
+import client.asta_lsi2.repository.ApprentiRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.Year;
+import java.util.List;
+
+@Service
 public class ApprentiService {
+
+	private final ApprentiRepository apprentiRepository;
+
+	public ApprentiService(ApprentiRepository apprentiRepository) {
+		this.apprentiRepository = apprentiRepository;
+	}
+
+	public List<Apprenti> listApprentisForYear(Year year) {
+		return apprentiRepository.findByYear(year);
+	}
+
+	public Apprenti save(Apprenti apprenti) {
+		return apprentiRepository.save(apprenti);
+	}
+
 }
